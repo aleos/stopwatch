6ae1e7e8b3147349571136eae35adc50befc5a7f
Alexander Ostrovsky <thealeos@gmail.com>
2022-08-18 10:27:00 +0300

    Add the iOS project

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Alexander Ostrovsky on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
